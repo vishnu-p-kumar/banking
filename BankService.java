d176c40a85f7b83fb5580a91f66d1ee1e87df22a
Vishnu P Kumar <160641455+vishnu-p-kumar@users.noreply.github.com>
2026-01-29 18:35:47 +0530

    Change withdrawal amount from 300 to 200

--- BankService.java
+++ BankService.java
@@ -27,7 +27,7 @@ public class BankService {
     public static void main(String[] args) {
         BankAccount acc = new BankAccount(1000);
         acc.deposit(500);
-        acc.withdraw(300);
+        acc.withdraw(200);
         System.out.println("Final Balance: " + acc.getBalance());
     }
 }
